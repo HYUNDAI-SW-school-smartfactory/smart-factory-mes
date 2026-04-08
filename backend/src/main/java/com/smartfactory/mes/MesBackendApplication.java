9eb7f2074b1fa7aa9cfd00bf740a64a40b231005
HanYunJi <hyj9908@gmail.com>
2026-04-08 17:45:22 +0900

    feat: initialize backend project with Spring Boot

---
+++ backend/src/main/java/com/smartfactory/mes/MesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartfactory.mes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesBackendApplication.class, args);
+	}
+
+}
